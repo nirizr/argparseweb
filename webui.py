3a06bfa6fc885344b304bd2d5a810383bf5ff0cf
Nir Izraeli <nir@sentinel-labs.com>
2015-02-26 15:03:27 +0200

    Made Webui.app the internal construction function
    dispatch is just another usage option and app manages all the defaulting
    
    Signed-off-by: Nir Izraeli <nir@sentinel-labs.com>

--- webui.py
+++ webui.py
@@ -12,6 +12,13 @@ class Webui(object):
     return ('/', 'index')
 
   def app(self, dispatch, parsed):
+    # Make sure we get an argh-like object here that has a dispatch object
+    if dispatch is None:
+      if not hasattr(self._parser, 'dispatch'):
+        raise ValueError("Can't dispatch a non dispatchable parser without a dispatch method")
+      dispatch = self._parser.dispatch
+      parsed = False
+
     class WebuiPageWrapper(page.WebuiPage):
       _parser = self._parser
       _dispatch = dispatch
@@ -23,17 +30,10 @@ class Webui(object):
     return web.application(urls, classes)
 
   def dispatch(self, dispatch=None, parsed=False):
-    # Make sure we get an argh-like object here that has a dispatch object
-    if dispatch is None and hasattr(self._parser, 'dispatch'):
-      dispatch = self._parser.dispatch
-
-    if dispatch is None:
-      raise ValueError("Can't dispatch a non dispatchable parser without a dispatch method")
-
     self.app(dispatch=dispatch, parsed=parsed).run()
 
   def wsgi(self, dispatch=None, parsed=True):
-    return self.dispatch(dispatch, parsed).wsgifunc()
+    return self.app(dispatch, parsed).wsgifunc()
 
   def get(self, count=True):
     # prepare a process-safe queue to hold all results
