d30724cf50ab6f114719118ceef99b465bfb5661
Nir Izraeli <nirizr@gmail.com>
2016-07-24 23:59:02 +0300

    fix setup.py
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- argparseweb/__init__.py
+++ argparseweb/__init__.py
@@ -1,2 +1,4 @@
 from .utils import *
 from .webui import *
+
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
+from os.path import exists
 from distutils.core import setup
-
-with open('requirements.txt') as f:
-  requirements = f.read().splitlines()
+import argparseweb
 
 setup(
   name = 'argparseweb',
   packages = ['argparseweb'],
-  version = '0.1.1',
+  version = argparseweb.__version__,
   description = 'Automatic exposure of argparse-compatible scripts as a web interface',
+  long_description=(open('README.md').read() if exists('README.md') else ''),
   author = 'Nir Izraeli',
   author_email = 'nirizr@gmail.com',
   url = 'https://github.com/nirizr/argparseweb',
-  download_url = 'https://github.com/nirizr/argparseweb/tarball/0.1.1',
+  download_url = 'https://github.com/nirizr/argparseweb/tarball/{}'.format(argparseweb.__version__),
   keywords = ['webui', 'web', 'user', 'interface', 'ui', 'argparse', 'webpy', 'web.py'],
-  install_requires = requirements,
   classifiers = [],
 )
