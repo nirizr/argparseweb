bb1ea641b5944856a2d05c548e859bcbee6d542c
Nir Izraeli <nirizr@gmail.com>
2017-01-10 10:47:05 +0200

    Include template files in setup package data
    resolves #3
    
    Signed-off-by: Nir Izraeli <nirizr@gmail.com>

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def get_version():
 setup(
   name = 'argparseweb',
   packages = ['argparseweb'],
+  package_data = {'argparseweb': ['templates/*.html']},
   version = get_version(),
   description = 'Automatic exposure of argparse-compatible scripts as a web interface',
   long_description=(open('README.md').read() if exists('README.md') else ''),
